a73f9be8449606397c51c18b7cd817eca5da5f07
Bill Geoghegan <bgeoghegan@cartrawler.com>
2020-10-14 21:00:09 +0100

    Testing iFrame data persistance with postmessage

--- JS/setItem.js
+++ JS/setItem.js
@@ -1,12 +1,19 @@
 const setValue = () => {
   window.onmessage = function (event) {
-    event.source.postMessage("This is a lovely message", event.origin);
+    event.source.postMessage(document.body.innerHTML, event.origin);
   };
 };
 
-const getValue = () => {
-  const value = new Date();
-  localStorage.getItem("VALUE");
-  console.log("got value: ", value);
-  document.getElementById("test").innerHTML = `Value: ${value}`;
+// const getValue = () => {
+//   const value = new Date();
+//   localStorage.getItem("VALUE");
+//   console.log("got value: ", value);
+//   document.getElementById("test").innerHTML = `Value: ${value}`;
+// };
+
+window.onmessage = function (event) {
+  // event.source.postMessage(document.body.innerHTML, event.origin);
+
+  var evt = event || window.event;
+  evt.source.postMessage("Message from iFrame", evt.origin);
 };
